6cdca9f760f82cf7e76576ad7e17b37ad7dabe2f
Nick Farrell <Nick.Farrell@genesiscare.com>
2021-11-11 13:43:00 +1100

    reload options better

--- options.js
+++ options.js
@@ -1,20 +1,19 @@
-var config = Object();
-
 function saveOptions(e) {
-  browser.storage.sync.set(
-    {
-      moveForeignTabs: document.querySelector("#move-foreign-tabs").value,
-      sortTabs: document.querySelector("#sort-tabs").value,
-      highlightTabs: document.querySelector("#highlight-tabs").value,
-    },
-    restoreOptions
-  );
+  browser.storage.sync.set({
+    moveForeignTabs: document.querySelector("#move-foreign-tabs").value,
+    sortTabs: document.querySelector("#sort-tabs").value,
+    highlightTabs: document.querySelector("#highlight-tabs").value,
+  });
   e.preventDefault();
 }
 
 function restoreOptions() {
-  browser.storage.sync.get("colour").then((res) => {
-    config = res;
+  browser.storage.sync.get().then((result) => {
+    document.querySelector("#move-foreign-tabs").checked =
+      result.moveForeignTabs || true;
+    document.querySelector("#sort-tabs").checked = result.sortTabs || false;
+    document.querySelector("#highlight-tabs").checked =
+      result.highlightTabs || false;
   });
 }
 
